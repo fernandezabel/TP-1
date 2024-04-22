db3f629790d54d5034942ca14d24df309f7c4305
fernandezabel <163901256+fernandezabel@users.noreply.github.com>
2024-04-22 13:53:14 -0300

    Add files via upload

---
+++ tp N°1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n1,n2,m;
+
+cout<<"ingrese base"<<endl;
+cin>>n1;
+cout<<"ingrese altura"<<endl;
+cin>>n2;
+
+m=n1*n2;
+
+cout<<"El resultado de la multiplicacion es:"<<m<<endl;
+	return 0;
+}
